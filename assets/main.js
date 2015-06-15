54ba53b6d84e8bd44b0e05260f301604f3ea95f0
dnguy <dnguy008@gmail.com>
2015-06-15 00:29:05 -0700

    completed modal

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,4 @@
 var item_array = [];
-var modal_item_display = [];
 
 function retrieve_info_images(){
 	$.ajax({
@@ -9,13 +8,27 @@ function retrieve_info_images(){
 		success: function(response){
 			item_array = response; 
 			for(var i = 0; i < item_array.length; i++){
-			var img_div = $('<div>').addClass('col-xs-3').attr({user_id: response[i].user_id, id: response[i].id});
+			var img_div = $('<div>').addClass('col-xs-3').attr({user_id: response[i].user_id, id: response[i].id, index_number: i});
 			var img = $('<img>').attr('src', response[i].filepath);
 			$(img_div).append(img);
 			$('.item_container').append(img_div);
 
 			$(img_div).click(function(){
-				var modal_img = $('<img>').attr('src', response[i].filepath)
+				$('.modal-title').html('');
+				$('.modal-body').html('');
+				console.log(item_array[$(this).attr('index_number')].filepath)
+				var modal_img = $('<img>').attr('src', item_array[$(this).attr('index_number')].filepath);
+				var title = $('<div>').text(item_array[$(this).attr('index_number')].title);
+				var shoe_condition = $('<div>').text('Condition: ' + item_array[$(this).attr('index_number')].shoe_condition)
+				var details = $('<div>').text(item_array[$(this).attr('index_number')].details);
+				var size = $('<div>').text('size: ' + item_array[$(this).attr('index_number')].size);
+				var price = $('<div>').text('$' + item_array[$(this).attr('index_number')].price);
+				var contact_buyer_button = $('<button>').attr('type', 'submit').text('Contact');
+
+				$('.modal-title').html(title);
+				$('.modal-body').append(modal_img,shoe_condition, details, size, price, contact_buyer_button);
+				$('#myModal').modal('show');
+
 			});
 		}
 		}
