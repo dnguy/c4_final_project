1c2a9116965e77327cef9ad95eec25dd9866a6a5
dnguy <dnguy008@gmail.com>
2015-06-29 14:20:50 -0700

    sql

--- assets/pages/account.php
+++ assets/pages/account.php
@@ -2,7 +2,7 @@
 session_start();
 ?>
 <div class="col-xs-10 col-xs-offset-1 account_container">
-<div class="col-xs-12"><h1>ACCOUNT<h1></div>
+<div class="col-xs-12"><h1>ACCOUNT <h1></div>
 <div class="col-xs-12">
 <?php 
 print_r('Name : ' . $_SESSION['name']);
