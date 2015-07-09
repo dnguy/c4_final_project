ca48864f82086105bb9dc1f4599fe10d66186bc0
dnguy <dnguy008@gmail.com>
2015-07-08 21:07:07 -0700

    added utf8 to messages

--- message_reply_handler.php
+++ message_reply_handler.php
@@ -16,8 +16,7 @@ if(mysqli_affected_rows($con) > 0){
 
 		$output['success'] = true; 
 }
-
-$output = utf8_encode($output);
+$output = array_map('utf8_encode', $output);
 $output_string = json_encode($output);
 print_r($output_string);
 ?>
